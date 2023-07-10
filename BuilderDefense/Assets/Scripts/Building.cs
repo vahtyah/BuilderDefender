4cf842c8c3c062d5383554b79d25b593b120a992
Май Ван Туан <8268826@gmail.com>
2023-07-10 09:54:44 +0700

    Building demolish

--- BuilderDefense/Assets/Scripts/Building.cs
+++ BuilderDefense/Assets/Scripts/Building.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 public class Building : MonoBehaviour
 {
     private HealthSystem _healthSystem;
+    private Transform _buildingDemolishBtn;
 
     public HealthSystem HealthSystem => _healthSystem;
 
     private void Awake()
     {
+        _buildingDemolishBtn = transform.Find("pfBuildingDemolishBtn");
+        if (_buildingDemolishBtn != null)
+        {
+            _buildingDemolishBtn.gameObject.SetActive(false);
+        }
+
         var buildingType = GetComponent<BuildingTypeHolder>().buildingType;
         _healthSystem = GetComponent<HealthSystem>();
         _healthSystem.SetHealthAmount(buildingType.healthAmountMax);
         _healthSystem.Died += (sender, args) => { Destroy(gameObject); };
+    }
+
+    private void OnMouseEnter()
+    {
+        if (_buildingDemolishBtn != null)
+        {
+            _buildingDemolishBtn.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (_buildingDemolishBtn != null)
+        {
+            _buildingDemolishBtn.gameObject.SetActive(false);
+        }
     }
 }
---
+++ BuilderDefense/Assets/Scripts/BuildingDemolishBtn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingDemolishBtn : MonoBehaviour
+{
+    [SerializeField] private Building _building;
+    private void Awake()
+    {
+        transform.Find("button").GetComponent<Button>().onClick.AddListener(() =>
+        {
+            var buildingTypeSo = _building.GetComponent<BuildingTypeHolder>().buildingType;
+            foreach (var resourceAmount in buildingTypeSo.constructionResourceCostArray)
+            {
+                ResourceManager.Instance.AddResource(resourceAmount.resourceType, Mathf.FloorToInt(resourceAmount.amount * .6f));
+            }
+            Destroy(_building.gameObject);
+        });
+    }
+}
